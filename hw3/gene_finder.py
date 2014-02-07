21bb9bc10090d3c6bd76970026e233e75e86b956
hzhu <hzhu@hzhu.(none)>
2014-02-07 17:01:54 -0500

    Backing up.

--- hw3/gene_finder.py
+++ hw3/gene_finder.py
@@ -2,7 +2,7 @@
 """
 Created on Sun Feb  2 11:24:42 2014
 
-@author: YOUR NAME HERE
+@author: Huanzhen David Zhu
 """
 
 # you may find it useful to import these variables (although you are not required to use them)
@@ -15,7 +15,6 @@ def collapse(L):
         output = output + s
     return output
 
-
 def coding_strand_to_AA(dna):
     """ Computes the Protein encoded by a sequence of DNA.  This function
         does not check for start and stop codons (it assumes that the input
@@ -25,13 +24,38 @@ def coding_strand_to_AA(dna):
         returns: a string containing the sequence of amino acids encoded by the
                  the input DNA fragment
     """
+    # Create a count for the DNA for easy segregation.
+    dna_count = range(0, len(dna),3)
+
+    # Initially declare an empty string amino that stores the translated amino acid.
+    amino = ''
     
-    # YOUR IMPLEMENTATION HERE
+    # Removes the last few nucleotides if there's not enough for one codon.
+    if (len(dna) - dna_count[-1]) % 3 != 0:
+        del dna_count[-1]
+
+    # Packages and translates aa to codons.
+    for nucleotide in dna_count:
+
+        # Initial split.
+        cod = dna[nucleotide:nucleotide+3]
+
+        # Translation part 1.
+        for i in range(len(codons)):
+            if cod in codons[i]:
+                amino = amino + aa[i]
+                break
+
+    # Return translation.
+    return amino
 
 def coding_strand_to_AA_unit_tests():
     """ Unit tests for the coding_strand_to_AA function """
-        
-    # YOUR IMPLEMENTATION HERE
+    inp = 'GACGTGGCACTGGCGT'
+    print "input: " + inp
+    print "expected output: DVALA"
+    print "actual output: " + coding_strand_to_AA(inp)
+# coding_strand_to_AA_unit_tests()
 
 def get_reverse_complement(dna):
     """ Computes the reverse complementary sequence of DNA for the specfied DNA
@@ -40,13 +64,34 @@ def get_reverse_complement(dna):
         dna: a DNA sequence represented as a string
         returns: the reverse complementary DNA sequence represented as a string
     """
+    # Initial reverse.
+    rev_dna_1 = dna[::-1]
+
+    # Final translation storage.
+    rev_dna_2 = ''
     
-    # YOUR IMPLEMENTATION HERE
+    # Translation.
+    for nucleotide in rev_dna_1:
+        if nucleotide == 'A':
+            rev_dna_2 = rev_dna_2 + 'T'
+        elif nucleotide == 'T':
+            rev_dna_2 = rev_dna_2 + 'A'
+        elif nucleotide == 'C':
+            rev_dna_2 = rev_dna_2 + 'G'
+        elif nucleotide == 'G':
+            rev_dna_2 = rev_dna_2 + 'C'
     
+    return rev_dna_2
+
 def get_reverse_complement_unit_tests():
     """ Unit tests for the get_complement function """
-        
-    # YOUR IMPLEMENTATION HERE    
+    print "input: ATGCCCGCTTT"
+    print "expected output: AAAGCGGGCAT"
+    print "acutal output: " + get_reverse_complement("ATGCCCGCTTT")
+    print "input: CCGCGTTCA"
+    print "expected output: TGAACGCGG"
+    print "actual output: " + get_reverse_complement("CCGCGTTCA") 
+# get_reverse_complement_unit_tests()
 
 def rest_of_ORF(dna):
     """ Takes a DNA sequence that is assumed to begin with a start codon and returns
@@ -56,13 +101,29 @@ def rest_of_ORF(dna):
         dna: a DNA sequence
         returns: the open reading frame represented as a string
     """
+    # Define stop codons.
+    stops = ['TAG', 'TAA', 'TGA']
     
-    # YOUR IMPLEMENTATION HERE
+    # Definte an empty array for storing stop codon positions.
+    lowest = []
+    
+    # Find and test the earliest stop codon.
+    for stop in stops:
+        if stop in dna:
+            if dna.index(stop) % 3 == 0:
+                lowest.append(dna.index(stop))
+    
+    return dna[0:min(lowest)]
 
 def rest_of_ORF_unit_tests():
     """ Unit tests for the rest_of_ORF function """
-        
-    # YOUR IMPLEMENTATION HERE
+    print "input: ATGTGAA"
+    print "expected output: ATG"
+    print "actual output: " + rest_of_ORF("ATGTGAA")
+    print "input: ATGAGATAGG"
+    print "expected output: ATGAGA"
+    print "acutal output: " + rest_of_ORF("ATGAGATAGG")
+# rest_of_ORF_unit_tests()
         
 def find_all_ORFs_oneframe(dna):
     """ Finds all non-nested open reading frames in the given DNA sequence and returns
---
+++ hw3/sandbox.py
@@ -0,0 +1,10 @@
+# something  = "something"
+# print something[0:3]
+# print len(something)
+
+
+# for y in range(0, len(something), 2):
+#     print y
+
+string = "test"
+print string.index('est')
